6d237cd825d1cc471b61fc4d595f9134e89bfd48
iamara <iamarabarrios457@gmail.com>
2023-06-14 12:18:26 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - imprimir
+ *
+ * Return: Always 0 (return)
+ **/
 /* betty style doc for function main goes there */
 int main(void)
 {
