8f84f9bc167ef512331d3d15a2ec18cc99e6620f
iamara <iamarabarrios457@gmail.com>
2023-06-15 06:10:19 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -15,27 +15,3 @@ putchar('a' + i);
 putchar('\n');
 return (0);
 }
-
-
-
-
-Escribe un programa que imprima todas las combinaciones posibles de números de un solo dígito.
-
-Los números deben ir separados por #include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-/**
-* main - imprimir
-*
-* Return: Always 0 (return)
-**/
-int main(void)
-{
-int i;
-for (i = 48; i < 57; i++)
-putchar(i);
-putchar(", ");
-putchar('\n');
-return (0);
-}
