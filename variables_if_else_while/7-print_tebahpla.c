c940c95c9ea44017be29acb6365d5f9a7f320b4c
iamara <iamarabarrios457@gmail.com>
2023-06-14 18:41:19 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - imprimir
+*
+* Return: Always 0 (return)
+**/
+int main(void)
+{
+int i;
+for (i = 26; i < 0; i++)
+putchar(a + i);
+putchar('\n');
+return (0);
+}
