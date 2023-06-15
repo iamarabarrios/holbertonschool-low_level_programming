17f1531489424472000ff0f7a4f6ad5377a6600b
iamara <iamarabarrios457@gmail.com>
2023-06-14 18:16:11 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - imprimir
+*
+* Return: Always 0 (return)
+**/
+int main(void)
+{
+putchar(48);
+putchar(49);
+putchar(50);
+putchar(51);
+putchar(52);
+putchar(53);
+putchar(54);
+putchar(55);
+putchar(56);
+putchar(57);
+putchar('\n');
+return (0);
+}
