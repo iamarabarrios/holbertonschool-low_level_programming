0b9ddf1ad03fe920b418513ece5f31cba67da5c3
iamara <iamarabarrios457@gmail.com>
2023-06-15 06:03:41 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - imprimir
+*
+* Return: Always 0 (return)
+**/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i+48);
+for (i = 97; i < 103; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
