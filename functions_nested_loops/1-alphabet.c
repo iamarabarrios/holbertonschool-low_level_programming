56aa6f415a33d2c786d2fdce29bc03d9272ba0ab
iamara <iamarabarrios457@gmail.com>
2023-06-15 10:02:06 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* main - imprimir
+*
+* Return: Always 0 (return)
+**/
+void print_alphabet(void);
+{
+int i;
+for (i = 97; i < 122; i++)
+putchar('0' + i);
+putchar('\n');
+return (0);
+}
