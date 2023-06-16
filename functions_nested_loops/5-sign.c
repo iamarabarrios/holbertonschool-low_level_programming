6e9548703642840e7cd5015c664fff1963fb2d6a
iamara <iamarabarrios457@gmail.com>
2023-06-16 07:18:11 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,21 +10,21 @@
 **/
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
 _putchar(43);
 return (1);
 }
-else if(n == 0)
+else if (n == 0)
 {
 _putchar(48);
 return (0);
 }
-else if(n < 0)
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
 }
 putchar('\n');
-return(0);
+return (0);
 }
