b0b6140a308d9d26a8fdc9ef6c0815a8738fa0dd
iamara <iamarabarrios457@gmail.com>
2023-06-16 07:57:15 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* _abs - calcula el valor absoluto
+* @n:absolut
+* Return: Always 0 (return)
+**/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * (-1));
+}
+return (n);
+}
