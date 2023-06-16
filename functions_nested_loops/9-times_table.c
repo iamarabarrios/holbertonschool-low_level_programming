b1a37bd28696bb623b66df8c6dba534b77920b15
iamara <iamarabarrios457@gmail.com>
2023-06-16 10:20:55 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* times_table - imprime tabla del 9
+*
+* Return: Always 0 (return)
+**/
+void times_table(void)
+{
+int number = 9
+_putchar(number * '0');
+_putchar(number * '1');
+_putchar(number * '2');
+_putchar(number * '3');
+_putchar(number * '4');
+_putchar(number * '5');
+_putchar(number * '6');
+_putchar(number * '7');
+_putchar(number * '8');
+_putchar(number * '9');
+_putchar(number * '9' + '1');
+}
