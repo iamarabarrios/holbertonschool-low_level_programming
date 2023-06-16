e4bd6692feb105f77d84237583431ebcdbb702c0
iamara <iamarabarrios457@gmail.com>
2023-06-16 10:11:07 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,8 +12,8 @@ void jack_bauer(void)
 {
 int hour;
 int minute;
-for(hour = 0; <= 23; hour++)
-for(minute = 0; <= 59; minute++)
+for(hour = 0; hour <= 23; hour++)
+for(minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
