5632f42a57fb56b07644e7168710b1852d197800
iamara <iamarabarrios457@gmail.com>
2023-06-16 06:28:24 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* int _islower(int c) - verificar si hay minusculas
+* @c:int
+* Return: Always 0 (return)
+**/
+int _islower(int c)
+{
+if(c >= 97 && c <= 122)
+return(1);
+return(0);
+}
