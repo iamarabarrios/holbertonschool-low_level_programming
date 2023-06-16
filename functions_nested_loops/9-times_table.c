09d45cca8395f89ce0d3a1e88376b0151108e24a
iamara <iamarabarrios457@gmail.com>
2023-06-16 12:16:33 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,16 +10,33 @@
 **/
 void times_table(void)
 {
-int number = 9;
-_putchar(number * '0');
-_putchar(number * '1');
-_putchar(number * '2');
-_putchar(number * '3');
-_putchar(number * '4');
-_putchar(number * '5');
-_putchar(number * '6');
-_putchar(number * '7');
-_putchar(number * '8');
-_putchar(number * '9');
-_putchar(number * '9' + '1');
+int i;
+int j;
+int firtsdigit;
+int seconddigit;
+int result;
+for (i = 0; i <10; i++)
+{
+for (j = 0; j <10; j++)   
+{
+result = i * j;
+firstdigit = result / 10;
+seconddifit = result % 10;
+if (result <= 9 && j != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+if (result > 9 && j != 0)
+{
+_putchar(44);
+_putchar(32);
+}
+if (result > 9)
+_putchar(firstdigit + 48);
+_putchar(seconddigit + 48); 
+}
+_putchar('\n');
+}
 }
