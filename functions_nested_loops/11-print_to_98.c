9f1daf8fb9f410d2cd5cda9bd33aff7652124330
iamara <iamarabarrios457@gmail.com>
2023-06-18 14:58:18 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,30 +9,25 @@
 * Return: Always 0 (return)
 **/
 void print_to_98(int n)
+{
 int i;
 if (n < 98)
-{
-for (i = n; i < 99; i++)
-{
+for (i = n; i < 99; i++);
 if (i != n)
 {
 printf(", ");
 printf("%i", i);
 }
-}
-}
 if (n > 98)
 {
-for (i = 0; i > 98; i++)
-}
-{
-for (i = 0; i > 98; i--)
-{
-for (", ")
-}
+for (i = 0; i > 98; i--);
+ printf(", ");
+ printf("%i", i);
 }
 if (n == 98)
 {
 printf("98");
 _putchar('\n');
 }
+}
+
