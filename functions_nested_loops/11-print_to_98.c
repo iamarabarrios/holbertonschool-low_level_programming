4ba95aa1edd252c16025860b81a3c4e8be515d84
iamara <iamarabarrios457@gmail.com>
2023-06-18 09:50:54 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+*  print_to_98 - imprime los naturales
+*@n:naturales
+* Return: Always 0 (return)
+**/
+void print_to_98(int n)
+{
+int i;
+int j;
+int firstdigit;
+int seconddigit;
+int result;
+for (i = 0; i < 98; i++)
+{
+_putchar(44);
+_putchar(32);
+}
+_putchar('\n');
+}
