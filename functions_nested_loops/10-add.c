cd4c7b019215da70ec7f52a9c4efe9069ebc5b66
iamara <iamarabarrios457@gmail.com>
2023-06-18 08:29:24 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* add - imprime dos enteros y devueve el resultado
+*
+* Return: Always 0 (return)
+**/
+int add(int, int)
+{
+int i;
+int j;
+int result;
+i = 2;
+j = 4;
+result = i + j;
+_putchar(i + j = result);
