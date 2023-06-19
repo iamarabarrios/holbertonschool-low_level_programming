5315ef78b04cc1f51e5e57e88494d19934dd6a0f
iamara <iamarabarrios457@gmail.com>
2023-06-19 10:50:12 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* _isupper - verificar si hay mayusculas
+* @c:int
+* Return: Always 0 (return)
+**/
+int _isupper(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+return (0);
+}
