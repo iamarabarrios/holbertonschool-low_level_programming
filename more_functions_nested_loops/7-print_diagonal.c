aa066e501334c7a88802a1dbcd3c3ca9ce0560da
iamara <iamarabarrios457@gmail.com>
2023-06-20 06:56:23 -0700

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,28 @@
-hacer 
+#include "main.h"
+/**
+* print_diagonal - imprime diagonal
+*@n:int
+* Return: Always 0 (return)
+**/
+void print_diagonal(int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (n > 0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+		_putchar('\n');
+}
