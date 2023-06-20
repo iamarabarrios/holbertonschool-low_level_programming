bf131bdb724bac4de70a7202b455344699889b03
iamara <iamarabarrios457@gmail.com>
2023-06-20 16:59:50 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - imprime la frase
- * Return: 1
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful"
-	"\" - Dora Korpar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
+
