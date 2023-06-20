740a99f95aceea44ef491a84b154800e72f47067
iamara <iamarabarrios457@gmail.com>
2023-06-20 05:01:46 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
 **/
 void print_most_numbers(void)
 {
-	int i;
-
-	for (; i <= 9; i++)
-	{
-		if (i != 2 || i != 4)
-		{
-			putchar(i);
-		}
-	}
-	putchar('\n');
+int i;
+for (; i <= 9; i++)
+{
+if (i != 2 || i != 4)
+{
+putchar(i);
+}
+}
+putchar('\n');
 }
