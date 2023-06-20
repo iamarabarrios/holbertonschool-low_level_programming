358ecfebe98901c10e3bcee5b7caa9d050ba2609
iamara <iamarabarrios457@gmail.com>
2023-06-20 07:05:40 -0700

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,27 @@
-hacer
+#include "main.h"
+/**
+* print_square - imprime square
+*@size:int
+* Return: Always 0 (return)
+**/
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
