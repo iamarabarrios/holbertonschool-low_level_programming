7eaf588091e14a9b20846ce2a5232c579772e3e6
iamara <iamarabarrios457@gmail.com>
2023-06-19 19:22:32 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,24 @@
-hacer 
+#include "main.h"
+/**
+* print_most_numbers - verificar si hay mayusculas
+*
+* Return: Always 0 (return)
+**/
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+	if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+}
+
+	_putchar('\n');
+}
