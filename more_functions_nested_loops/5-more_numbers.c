e6e8e0d323daab344af3c23a2f7cc0349504e4be
iamara <iamarabarrios457@gmail.com>
2023-06-19 19:48:52 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,29 @@
-hacer 
+ #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* more_numbers - imprime 10 veces del 0 al 14
+*
+* Return: Always 0 (return)
+**/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+
+			_putchar((j % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
