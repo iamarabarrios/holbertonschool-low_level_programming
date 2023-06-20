c9a97e720c4ec74d378ce15a09fa9aa9e651b522
iamara <iamarabarrios457@gmail.com>
2023-06-19 20:08:57 -0700

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,27 @@
-hacer 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+* print_line - imprime linea
+*@n:int
+* Return: Always 0 (return)
+**/void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar(95);
+		}
+
+		_putchar('\n');
+	}
+} 
