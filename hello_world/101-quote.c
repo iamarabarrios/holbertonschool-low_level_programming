75bd2b7f058f9ec4409dbb7470c7ce745d0bc5f9
iamara <iamarabarrios457@gmail.com>
2023-06-20 16:01:05 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - imprime la frase
- * Return:0
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
 }
