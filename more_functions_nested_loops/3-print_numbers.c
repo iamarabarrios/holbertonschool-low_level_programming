2eb7893f049fda162ab93416363a5193f81747d6
iamara <iamarabarrios457@gmail.com>
2023-06-19 19:42:25 -0700

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
-hacer 
+#include "main.h"
+/**
+* print_numbers - imprime del 0 al 9 uno por linea
+*
+* Return: Always 0 (return)
+**/
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
+
