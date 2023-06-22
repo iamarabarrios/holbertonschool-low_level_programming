ba3d86f3e41d9edc905acda6b6a3607f4a254233
iamara <iamarabarrios457@gmail.com>
2023-06-22 12:04:58 -0700

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,15 @@
-hacer
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - longitud
+ * @s: int
+ *
+ * Return: void
+ *
+ */
+void rev_string(char *s)
+{
+int i;
+	for (i = 0; s[i] != '\0'; i++)
+}
