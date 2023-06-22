2d6300e6f1a0a4bd6eca51cd0f8f99c0aabff1b0
iamara <iamarabarrios457@gmail.com>
2023-06-22 07:18:16 -0700

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
-hacer
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - imprime
+ * @str: int
+ *
+ * Return: void
+ *
+ */
+void _puts(char *str)
+{
+int i;
+int count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	count++;
+	_putchar(count);
+}
