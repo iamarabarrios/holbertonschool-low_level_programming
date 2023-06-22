1d71d6911f7d2a5860ba55afeb389618b9e1b274
iamara <iamarabarrios457@gmail.com>
2023-06-22 05:37:51 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - punteros
+ * @n: int
+ *
+ * Return: On success 1.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
