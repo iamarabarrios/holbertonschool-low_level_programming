b045d8230ab3aa4daf0cb39ee090c3f1a6151aa6
iamara <iamarabarrios457@gmail.com>
2023-06-22 16:57:52 -0700

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,8 +12,11 @@ void rev_string(char *s)
 
 {
 int i;
-for (i = 0; s[i] == '\0'; i--)
-{
 
-}
+	for (i = 0; s[i] == '\0'; i--)
+	{
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	}
 }
