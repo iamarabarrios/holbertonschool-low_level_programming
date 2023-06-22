12c08fe90c5ce68916868aaa78474f9374368494
iamara <iamarabarrios457@gmail.com>
2023-06-22 06:14:14 -0700

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,1 +1,18 @@
-haacerr
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - punteros a por b
+ * @a: int
+ * @b: int
+ * Return: void
+ *
+ */
+void swap_int(int *a, int *b)
+{
+int value1 = *a;
+int value2 = *b;
+
+	*b = value1;
+	*a = value2;
+}
