e4afb366ed57d2a9dc27a3f058e8e524463fc372
iamara <iamarabarrios457@gmail.com>
2023-06-22 06:48:30 -0700

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
-hacer
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - longitud
+ * @s: int
+ *
+ * Return: void
+ *
+ */
+int _strlen(char *s)
+{
+int i;
+int count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	count++;
+	return (count);
+}
+
