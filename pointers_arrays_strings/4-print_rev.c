f2040a55f793976469334991391c73f8e16ba9b7
iamara <iamarabarrios457@gmail.com>
2023-06-22 10:25:59 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,18 @@
-hacer
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - imprime al reves
+ * @s: char
+ *
+ * Return: void
+ *
+ */
+void print_rev(char *s)
+{
+int i;
+int count = 0;
+for (i = '\0'; s[i] != 0; i--)
+count--;
+	_putchar(count);
+}
