9410e3b5cd40f76656bd36c4e4debc1ed0c21100
iamara <iamarabarrios457@gmail.com>
2023-06-22 15:55:53 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,18 @@
-hacer
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - imprime cada dos
+ *@str: char
+ *
+ * Return: void
+ *
+ */
+void puts2(char *str)
+{
+int i;
+int count = 0;
+	for (i = 0; str[i] != '\0'; i += 2)
+	count++;
+	_putchar (count);
+}
