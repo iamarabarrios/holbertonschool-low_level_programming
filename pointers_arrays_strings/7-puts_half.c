47e1783e0cb0f589b4c7910f89e4c1d9d1338075
iamara <iamarabarrios457@gmail.com>
2023-06-23 06:18:58 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,11 @@ void puts_half(char *str)
 int i;
 int size = 0;
 
-while (str[size] != '/0')
+while (str[size] != '\0')
 {
 size++;
 }
-for (i = size/2; str[i] != '\0'; i++)
+for (i = size / 2; str[i] != '\0'; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
