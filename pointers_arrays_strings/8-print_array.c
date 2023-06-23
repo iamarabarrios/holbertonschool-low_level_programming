4feba72c2a8ecfd187bf5bb02de7330eabada2f6
iamara <iamarabarrios457@gmail.com>
2023-06-23 09:44:50 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,19 @@
-hacer
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - imprime array
+ * @a: int
+ * @n: int
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		_putchar(44);
+		_putchar(32);
+	}
+	_putchar('\n');
+}
