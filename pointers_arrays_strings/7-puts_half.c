82ac475be89d5b185cb8a72c259d82de1994cca5
iamara <iamarabarrios457@gmail.com>
2023-06-23 06:16:49 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - imprime la mitad
+ * @str: int
+ *
+ * Return: void
+ *
+ */
+void puts_half(char *str)
+{
+int i;
+int size = 0;
 
+while (str[size] != '/0')
+{
+size++;
+}
+for (i = size/2; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
 
