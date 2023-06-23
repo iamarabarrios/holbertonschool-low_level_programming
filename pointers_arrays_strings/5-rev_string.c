a8c276cc94c7286622fc1ccd5d7c31a87cf8ec2a
iamara <iamarabarrios457@gmail.com>
2023-06-22 21:09:10 -0700

    several fixes

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen - counts the length of a string
+ * @s: the string to count
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	for (; s[count] != '\0';)
+		count++;
+	return (count);
+}
+/**
  * rev_string - al reves
  * @s: int
  *
@@ -8,12 +21,12 @@
  */
 void rev_string(char *s)
 {
-int i;
-int size = _strlen(s);
+	int i;
+	int size = _strlen(s);
 
-	for (i = size - 1; i >= 0; i--)
+	for (i = size; i >= 0; i--)
 	{
-	_putchar(s[i]);
+		_putchar(s[i]);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
