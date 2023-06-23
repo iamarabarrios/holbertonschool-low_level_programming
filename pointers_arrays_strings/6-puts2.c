f8cc52e0e55507e11f4af2092d698bea5450ed9b
iamara <iamarabarrios457@gmail.com>
2023-06-23 05:56:21 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,12 @@
 void puts2(char *str)
 {
 int i;
-
-for (i = 0; str[i] != '\0'; i += 2)
+int count = 1;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (count % 2 != 0)
 _putchar(str[i]);
+count++;
+}
 _putchar('\n');
 }
