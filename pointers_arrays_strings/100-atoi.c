caa4795b29f023c71e2384cb8fcb22c5811e0e81
iamara <iamarabarrios457@gmail.com>
2023-06-24 12:48:04 -0700

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,37 @@
-hacer
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - convierta cadena en numero entero
+ * @s: char
+ *
+ * Return: void
+ *
+ */
+int _atoi(char *s)
+{
+int sign = 1;
+int result = 0;
+
+	while (*s == ' ')
+	{
+	s++;
+	}
+
+	if (*s == '-')
+	{
+	sign = -1;
+	s++;
+	}
+	else if (*s == '+')
+	{
+	s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+	result = result * 10 + (*s - '0');
+	s++;
+	}
+
+	return (result * sign);
+}
