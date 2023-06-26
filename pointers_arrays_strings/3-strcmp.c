a5728d968bbf4394580b0dda2b7f3ce33c62fb03
iamara <iamarabarrios457@gmail.com>
2023-06-26 10:16:29 -0700

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,26 @@
-hacer 
+#include "main.h"
+/**
+ * _strcmp - compara dos cadenas
+ * @s1: punt char
+ * @s2: punt char
+ * Return: dest
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+int bool = 0;
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+	if (*s1 != *s2)
+	{
+	bool = 1;
+	}
+	s1++;
+	s2++;
+	}
+
+	if (bool == 0)
+	return (0);
+	else
+	return (1);
+}
