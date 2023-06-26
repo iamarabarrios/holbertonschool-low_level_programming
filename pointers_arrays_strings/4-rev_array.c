ae1e9a7c7f49eb2291b8d2c13198b6d32d1ef101
iamara <iamarabarrios457@gmail.com>
2023-06-26 14:05:37 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,28 @@
-hacer
+#include "main.h"
+#include <string.h>
+/**
+ *reverse_array- imprime al reves
+ *@n: string
+ *@a: puntero
+ *Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	{
+	char m;
+	char j;
+	char i;
+
+	for (i = a[n]; i != '\0'; ++i)
+	;
+	if (i > n)
+	--i;
+
+	for (j = n; a[n] < i; ++j, --i)
+	{
+	m = j;
+	j = i;
+	i = m;
+	}
+	}
+}
