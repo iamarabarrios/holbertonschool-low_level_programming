78d9d8465ed79d29111506239d696567974c01c2
iamara <iamarabarrios457@gmail.com>
2023-06-26 09:16:30 -0700

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
-hacer 
+#include "main.h"
+/**
+ * _strncpy - copia una cadena
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+dest[i] = '\0';
+
+	return (dest);
+}
