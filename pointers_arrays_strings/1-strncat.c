3670da1e176587de0c7041ed9ef895ba00afb48f
iamara <iamarabarrios457@gmail.com>
2023-06-26 09:00:20 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,25 @@
-hacer
+#include "main.h"
+/**
+ * _strncat - concatena dos cadenas
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int length;
+
+	while (dest[length])
+		length++;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[length++] = src[i++];
+	}
+	dest[length] = '\0';
+
+	return (dest);
+}
