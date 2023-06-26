dda8c13e9a1141d00294683025d6ec7cb1bbef45
iamara <iamarabarrios457@gmail.com>
2023-06-26 11:16:37 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,8 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int result = s1 - s2;
+	while (s1 && s2)
+		if (s1 != s2)
+		{
+			return (s1 - s2);
+		}
+	s1++;
+	s2++;
 
-	return (result);
+	return (s1 - s2);
 
 }
