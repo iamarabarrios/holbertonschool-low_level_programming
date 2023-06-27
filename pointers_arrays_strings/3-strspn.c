da5681acc40773bf522982fbe1f575222d32f727
iamara <iamarabarrios457@gmail.com>
2023-06-27 14:43:07 -0700

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,32 @@
-hacer
+#include "main.h"
+/**
+ * _strspn - largo
+ *@s: punt char
+ *@accept: punt char
+ * Return: numero de bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0, flag;
+	char *principio = accept;
+
+	while (*s)
+	{
+		flag = 0;
+		while (*accept)
+		{
+			if (*accept == *s)
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = principio;
+		if (flag == 0)
+			break;
+	}
+	return (count);
+}
