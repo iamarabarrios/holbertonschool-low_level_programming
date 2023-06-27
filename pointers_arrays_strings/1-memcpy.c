7ee097499c022ef515f09e3a241192406b417b13
iamara <iamarabarrios457@gmail.com>
2023-06-27 05:29:05 -0700

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,19 @@
-hacer 
+#include "main.h"
+/**
+ * _memcpy - llena la memoria
+ * @dest: punt char
+ * @src: punt char
+ * @n:int
+ * Return: void
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
