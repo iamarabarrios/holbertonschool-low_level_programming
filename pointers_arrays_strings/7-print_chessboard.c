c2f26b9ae2d81bac875ce0a3367defe7c1dea80e
iamara <iamarabarrios457@gmail.com>
2023-06-27 16:01:05 -0700

    7-print_chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,22 @@
-hacer
+#include "main.h"
+/**
+ * print_chessboard - tablero de ajedrez
+ * @a: punt char
+ * Return: void
+ */
+#include <stdio.h>
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%c ", a[row][col]);
+		}
+		printf("\n");
+	}
+}
