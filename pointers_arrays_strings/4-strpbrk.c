3249140a21b5a4762ab02d59f8620c3f9e40e996
iamara <iamarabarrios457@gmail.com>
2023-06-27 15:10:22 -0700

    4-strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,25 @@
-hacer
+#include "main.h"
+/**
+ * _strpbrk - lenght and bytes
+ * @s: punt char
+ * @accept: punt char
+ * Return: NULL
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *p = accept;
+
+		while (*p)
+		{
+			if (*p == *s)
+				return (s);
+			++p;
+		}
+		++s;
+	}
+	return (NULL);
+
+}
