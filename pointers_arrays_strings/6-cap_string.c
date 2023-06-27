ab95f49026dd2834995d77d8ec43e6b4f5b940fc
iamara <iamarabarrios457@gmail.com>
2023-06-26 17:57:52 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,19 @@
-hacer 
+#include "main.h"
+/**
+ * cap_string - cambia a mayus
+ * @a: punt char
+ * Return: void
+ *
+ */
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		a[i] = (int)a[i] - 32;
+	}
+	return (a);
+
+}
