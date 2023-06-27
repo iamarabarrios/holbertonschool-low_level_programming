65fd6c33d99e3686eab32a6276c7510b4ccdf9b1
iamara <iamarabarrios457@gmail.com>
2023-06-26 19:31:08 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - llena la memoria
+ * @s: punt char
+ * @b: char
+ * @n:int
+ * Return: void
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+		return (s);
+
+}
--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,17 +2,35 @@
 /**
  * cap_string - cambia a mayus
  * @a: punt char
- * Return: void
+ * Return: s
  *
  */
 char *cap_string(char *a)
 {
 	int i;
 
-	for (i = 0; a[i] != '\0'; i++)
+	while (!(a[i] >= 'a' && a[i <= 'z']))
 	{
-		if (a[i] >= 'a' && a[i] <= 'z')
-		a[i] = (int)a[i] - 32;
+		i++;
+	if (a[i - 1] == ' ' ||
+		a[i - 1] == '\t' ||
+		a[i - 1] == '\n' ||
+		a[i - 1] == ',' ||
+		a[i - 1] == ';' ||
+		a[i - 1] == '.' ||
+		a[i - 1] == '!' ||
+		a[i - 1] == '?' ||
+		a[i - 1] == '"' ||
+		a[i - 1] == '(' ||
+		a[i - 1] == ')' ||
+		a[i - 1] == '{' ||
+		a[i - 1] == '}' ||
+		i == 0)
+	{
+		a[i] -= 32;
 	}
-	return (a);
+	i++;
+}
+return (a);
+
 }
