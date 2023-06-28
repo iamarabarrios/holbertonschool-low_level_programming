0613bf06b475b57cfedb0fb5782896280dd954e6
iamara <iamarabarrios457@gmail.com>
2023-06-28 07:55:25 -0700

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
- * print_chessboard - tablero de ajedrez
- * @a: punt char
- * Return: void
+ * factorial - factorial
+ * @n: int
+ * Return: n
  */
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	if (n == 0)
+	return (1);
+	else
+	return (n * factorial(n - 1));
+}
