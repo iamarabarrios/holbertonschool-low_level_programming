aebcbf16462cd916ed58aa48856f122639e9b1b3
iamara <iamarabarrios457@gmail.com>
2023-06-28 08:39:16 -0700

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,6 +1,15 @@
 #include "main.h"
 /**
- * print_chessboard - tablero de ajedrez
- * @a: punt char
- * Return: void
- */
+ * _pow_recursion - potencia
+ * @x: int
+ * @y: int
+ * Return: x
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	return (1);
+	if (y < 0)
+	return (_pow_recursion(x, y + 1) / x);
+	return (_pow_recursion(x, y - 1) * x);
+}
