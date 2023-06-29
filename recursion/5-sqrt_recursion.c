632652057616f0776c82e1a16df25a43769964df
iamara <iamarabarrios457@gmail.com>
2023-06-29 14:50:09 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * _sqrt - raiz cuadrada sqrt
- * @n: int
- * @m: int
- * Return: 0
-*/
-
+ *_sqrt - calcula raiz cuadrada
+ *@n: int
+ *@m: int
+ *Return: raiz cuadrada
+ */
 int _sqrt(int n, int m)
 {
 	int sqrt = m * m;
 		if (sqrt == n)
 			return (m);
-		if (sqrt > m)
+		if (sqrt > n)
 			return (-1);
 		return (_sqrt(n, m + 1));
+
 }
 
 /**
- * * _sqrt_recursion - raiz cuadrada
- * @n: int
- * Return: 0
+ *_sqrt_recursion - retorna la raiz cuadrada
+ *@n: int
+ *
+ *Return: raiz cuadrada
  */
-
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
