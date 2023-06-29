ce8d42e1f500413403032219be56e890fdc4197e
iamara <iamarabarrios457@gmail.com>
2023-06-29 15:33:23 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - imprime su nombre
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
