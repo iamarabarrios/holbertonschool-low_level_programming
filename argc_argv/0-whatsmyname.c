0429b621f4c244bf207086977ca00d5fd6b7cb3d
iamara <iamarabarrios457@gmail.com>
2023-06-30 06:17:35 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,8 +6,14 @@
  * @argv: char
  * Return: 0
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
 	printf("%s", argv[0]);
+	printf("\n");
+	}
 	return (0);
 }
