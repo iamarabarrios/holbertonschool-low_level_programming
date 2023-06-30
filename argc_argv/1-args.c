f3ae9875e47670ddc5d02c50dee3ac3ed702adf4
iamara <iamarabarrios457@gmail.com>
2023-06-30 06:24:39 -0700

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,1 +1,19 @@
-hacer
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - imprime su nombre
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s", argv[i]);
+	printf("\n");
+	}
+	return (0);
+}
