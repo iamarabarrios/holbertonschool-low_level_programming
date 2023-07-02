cfc69293d259068d99080746b619a8d930d8119f
iamara <iamarabarrios457@gmail.com>
2023-07-01 17:25:17 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,25 @@
-hacer
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - multiplica numeros
+ *@argc: int
+ *@argv: char
+ *Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	if (argc < 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i * j);
+	return (0);
+
+}
