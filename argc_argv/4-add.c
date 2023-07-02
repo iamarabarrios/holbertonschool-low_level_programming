d5f9f4d50d5eb1371ba70c7e3ef96e733a4a591b
iamara <iamarabarrios457@gmail.com>
2023-07-01 17:05:26 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,38 @@
-hacer
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - suma numeros positivos
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int count = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (b = 1; b <= argc; b++)
+	{
+		for (a = 0; a < argc; a++)
+		{
+			if (isdigit(argv[b][a]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+			else
+				count += atoi(argv[b]);
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
