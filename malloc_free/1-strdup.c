5eb4dd3a93447e7a3547214f79ac34b490e04989
iamara <iamarabarrios457@gmail.com>
2023-07-05 07:08:16 -0700

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,13 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  *_strdup - retorna un puntero a un espacio recien asignado
- *str: char
+ *@str: char
  *Return: puntero
  */
 
 char *_strdup(char *str)
 {
+	int size = strlen(str);
+	char *p = (char *)malloc(size + 1);
+	int i;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (p == NULL)/**p es el nuevo puntero*/
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = p[i];
+	}
+	return (p);
 }
