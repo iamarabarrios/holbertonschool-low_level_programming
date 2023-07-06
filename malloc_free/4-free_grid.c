fbe79638c9ed66530f921d55d2f034a392a26916
iamara <iamarabarrios457@gmail.com>
2023-07-06 12:08:19 -0700

    4-free_grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,18 +1,21 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * free_grid - libera una rejilla
  * @grid: int
  * @height: int
+ *Return: NULL
  */
 
 void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i < height; i++)
+	for  (i = 0; i < height; i++)
 	{
-		free(grid[i]);
+	free(grid[i]);
 	}
+
 	free(grid);
 }
+
