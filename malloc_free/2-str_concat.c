2f81542e07b1c3e2ab10fc44935a184357af34d3
iamara <iamarabarrios457@gmail.com>
2023-07-06 06:39:20 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	for (m = 0; s1[m] != '\0'; m++)
 		i++;
@@ -26,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	for	(m = 0; s2[m] != '\0'; m++)
 		j++;
 
-	str = malloc(sizeof(char *) * i + j + 1);
+	str = malloc(sizeof(char) * i + j + 1);
 
 	if (str == NULL)
 		return (NULL);
