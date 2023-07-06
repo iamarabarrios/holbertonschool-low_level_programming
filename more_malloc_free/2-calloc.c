ad39e51d9a76c3ff9f95013ebb047cb8b9372012
iamara <iamarabarrios457@gmail.com>
2023-07-06 14:17:44 -0700

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,1 +1,30 @@
-d
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_calloc - asigna memoria para un array
+ * @nmemb: int
+ * @size: int
+ * Return: puntero
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+	unsigned int count;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	count = 0;
+	while (count < nmemb * size)
+	{
+		str[count] = 0;
+		count++;
+	}
+	return (str);
+}
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,1 +1,36 @@
-j
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *array_range - crea una matriz de enteros
+ * @min: int
+ * @max: int
+ * Return: puntero
+ */
+
+int *array_range(int min, int max)
+{
+	int *str;
+	int arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	str = malloc(sizeof(int) * (max - min + 1));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	arr = 0;
+	while (min <= max)
+	{
+		str[arr] = min;
+		min++;
+		arr++;
+	}
+	return (str);
+}
