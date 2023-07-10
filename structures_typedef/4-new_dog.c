bb41b5b8b91f9d735a721cb373e91e3d9931f86a
iamara <iamarabarrios457@gmail.com>
2023-07-10 14:59:00 -0700

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,43 +1,49 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
- *new_dog - create a new dog
- *@name: char
- *@age: float
- *@owner: char
- *Return: doggy
- */
+* new_dog - create a new dog
+* @name: char
+* @age: float
+* @owner: char
+* Return: return doggy
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int nlen;
 	int olen;
 	int i;
-
 	dog_t *doggy;
 
-	nlen = olen = 0;
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
+	nlen = strlen(name) + 1;
+	olen = strlen(owner) + 1;
+
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
+	doggy->name = malloc(nlen * sizeof(char));
+	if (doggy->name == NULL)
+	{
+		free(doggy);
 		return (NULL);
+	}
 	for (i = 0; i < nlen; i++)
 		doggy->name[i] = name[i];
 
 	doggy->age = age;
 
-	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
+	doggy->owner = malloc(olen * sizeof(char));
+
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
 		return (NULL);
+	}
 	for (i = 0; i < olen; i++)
 		doggy->owner[i] = owner[i];
 
 	return (doggy);
-
 }
