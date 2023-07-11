b2d3b09b8bd4929103266afbb2d209262edeacd9
iamara <iamarabarrios457@gmail.com>
2023-07-11 06:49:45 -0700

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,8 @@
+#ifndef PRINT_NAME_H
+#define PRINT_NAME_H
 #include "function_pointers.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
 *print_name - print name
 *@name: pointer char
@@ -10,3 +13,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
+
+#endif
