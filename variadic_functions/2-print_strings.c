b9a139a2b65e937e73e27e705fd7453ed617ab41
iamara <iamarabarrios457@gmail.com>
2023-07-12 17:36:32 -0700

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		s = va_arg(list, char *);
 		if (!s)
-			printf("%s", s);
+			printf("nil");
 		else
 			printf("%s", s);
 		if (i < n - 1 && separator)
