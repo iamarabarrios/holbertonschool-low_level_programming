e9ef5c842179ac2b8ef3d1b4bdfadfab515235a7
iamara <iamarabarrios457@gmail.com>
2023-07-29 16:00:09 -0700

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,6 +11,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int size = sizeof(n) * 8 - 1;
 
-	return ((index > size) ? 1UL : ((n >> index) & 1));
-} /**si index es mayor que size, es decir si index esta fuera de rango*/
+	while (index > size)
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
+
+/**return ((index > size) ? -1 : ((n >> index) & 1));*/
+/**me dice que no se puede -1 pero si uso 1UL me devuelve 1 positivo*/
+/**si index es mayor que size, es decir si index esta fuera de rango*/
 /**return -1 por error y si no es asi return el value of bit*/
