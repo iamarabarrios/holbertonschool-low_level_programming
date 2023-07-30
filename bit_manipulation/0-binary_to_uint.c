e576fa4c78cb070e00a8eea8a7024c6c1d5cdf92
iamara <iamarabarrios457@gmail.com>
2023-07-30 13:54:50 -0700

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,9 +9,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal = 0;
 
+	if (*b == '\0')
+		return (0);
+
 	while (*b != '\0')
 	{
-		if ((*b != '0' && *b != '1') || *b == '\0')
+		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
@@ -19,5 +22,4 @@ unsigned int binary_to_uint(const char *b)
 		b++;
 	}
 	return (decimal);
-
 }
