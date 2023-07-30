09feae496e5cb78cadce5331e144d44c2ce8aedf
iamara <iamarabarrios457@gmail.com>
2023-07-30 14:28:55 -0700

    main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stddef.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
