9c36de3b1c060379f7e76d49103edf307aa3f3e9
iamara <iamarabarrios457@gmail.com>
2023-08-02 11:05:19 -0700

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,9 +12,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file;
 	int length = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
 	}
 	file = open(filename, O_WRONLY | O_APPEND);
 
