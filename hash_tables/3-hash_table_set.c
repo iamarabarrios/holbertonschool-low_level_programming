6d82f60f4e96eb3386ef26ba1d87396868e4de0c
iamara <iamarabarrios457@gmail.com>
2023-08-03 18:22:09 -0700

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,8 +8,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((unsigned char *) key,
-													ht->size);
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
 	hash_node_t *current = ht->array[index];
 	hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
@@ -21,17 +20,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(current->value, value) == 0)
 				return (1);
-		}
-
 	free(current->value);
 	current->value = malloc(strlen(value) + 1);
 
 	if (current->value == NULL)
 		return (0);
-	}
 	strcpy(current->value, value);
 	return (1);
+		}
 	current = current->next;
+	}
 
 	if (new_node == NULL)
 		return (0);
@@ -45,6 +43,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
+	strcpy(new_node->key, key);
+	strcpy(new_node->value, value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
