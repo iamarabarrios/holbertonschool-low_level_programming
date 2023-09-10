4973602532026a9a130badb42ff2fec23ccddff3
Iamara Barrios <124050709+iamarabarrios@users.noreply.github.com>
2023-09-10 20:46:55 +0000

    exercise 1-binary comma and betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,27 +2,34 @@
 
 /**
 * binary_search - searches for a value in a sorted array of integers
+*@array: is a pointer to the first element of the array to search in
 *@size: is the number of elements in array
 *@value: is the value to search for
 *Return: the index where value is located or -1
-*
 */
 
 int binary_search(int *array, size_t size, int value)
-{ 
+{
     int i;
     int left = 0;
-    int right = size - 1;
+    int right = (int)size - 1;
 
     while (left <= right)
     {
         int mid = left + (right - left) / 2;
 
-        printf("Searching in array[%d:%d]: ", left, right);
+        printf("Searching in array: ");
 
         for (i = left; i <= right; i++)
         {
-            printf("%d ", array[i]);
+            if (i == right)
+            {
+                printf("%d", array[i]);
+            }
+            else
+            {
+                printf("%d, ", array[i]);
+            }
         }
 
         printf("\n");
