015a313342c5ccbbaaa93d349dd5d6400e376033
iamara <iamarabarrios457@gmail.com>
2023-09-10 12:59:08 -0700

    exercise 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
 *linear_search - searches for a value in an array of integers.
 *@array: is a pointer to the first element of the array to search in
 *@size: is the number of elements in array.
-*@value: is the value to search for.
+*@value: is the value to search for
 *Return: the first index where value is located or -1
 */
 
