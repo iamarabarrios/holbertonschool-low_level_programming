e8c4bf8ee4fd05eb53f3b068cb2ba5a016101c21
Iamara Barrios <124050709+iamarabarrios@users.noreply.github.com>
2023-09-10 15:51:32 +0000

    exercise 0-linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,14 +10,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
+    size_t i;
 
     if (array == NULL)
     {
         return (-1);
     }
 
-    for (i = 0; i < 0; i++)
+    for (i = 0; i < size; i++)
     {
         if (array[i] == value)
         {
